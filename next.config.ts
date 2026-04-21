9226172c56701d00e2335ac60a44d5711b771430
Desm00nt <desm00nt@users.noreply.github.com>
2026-04-21 14:12:56 +0000

    Deploy: GitHub Pages setup

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  /* config options here */
+  output: "export",
+  basePath: "/territoriya-my",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
